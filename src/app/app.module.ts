ce779c4b7c24c9db61ab790ff1ffdf4ea929154c
Roman Sorin <rmaximsorin@gmail.com>
2019-07-02 08:30:25 -0400

    Imports and base structuring of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CategoryListComponent } from "./components/category-list/category-list.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { HomeComponent } from "./components/home/home.component";
+import { MagentoComponent } from "./components/magento/magento.component";
+import { WordpressComponent } from "./components/wordpress/wordpress.component";
+import { ShopifyComponent } from "./components/shopify/shopify.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ProductComponent } from "./components/product/product.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "magento", component: MagentoComponent },
+  { path: "wordpress", component: WordpressComponent },
+  { path: "shopify", component: ShopifyComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    MagentoComponent,
+    WordpressComponent,
+    ShopifyComponent,
+    HeaderComponent,
+    ProductComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
